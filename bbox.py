807cf8b8ce23d21ee722975499019c1d9bee60b8
Sam Acquaviva <samacqua@gmail.com>
2021-07-30 15:48:13 -0400

    fixed bbox

--- bbox.py
+++ bbox.py
@@ -5,13 +5,13 @@ class BBox(Enum):
     X1Y1X2Y2 = auto()
     XMIDYMIDWH_NORM = auto()
 
-def bbox_to_xywh(bbox, format):
-    if format == BBox.XYWH:
+def bbox_to_xywh(bbox, from_format, image_size=(800, 600)):
+    if from_format == BBox.XYWH:
         x,y,w,h = bbox
-    elif format == BBox.X1Y1X2Y2:
+    elif from_format == BBox.X1Y1X2Y2:
         x,y,x2,y2 = bbox
         w, h = x2-x, y2-y
-    elif format == BBox.XMIDYMIDWH_NORM:
+    elif from_format == BBox.XMIDYMIDWH_NORM:
         x_mid_n, y_mid_n, w_n, h_n = bbox
         x_mid, w = x_mid_n * image_size[0], w_n * image_size[0]
         y_mid, h = y_mid_n * image_size[1], h_n * image_size[1]
@@ -21,7 +21,7 @@ def bbox_to_xywh(bbox, format):
 
 def convert_bbox(bbox, from_format, to_format, image_size=(800, 600)):
 
-    x,y,w,h = bbox_to_xywh(bbox, from_format)
+    x,y,w,h = bbox_to_xywh(bbox, from_format, image_size=image_size)
     
     if to_format == BBox.XYWH:
         return [x,y,w,h]
@@ -30,10 +30,16 @@ def convert_bbox(bbox, from_format, to_format, image_size=(800, 600)):
     elif to_format == BBox.XMIDYMIDWH_NORM:
         x_n, w_n = x/image_size[0], w/image_size[0]
         y_n, h_n = y/image_size[1], h/image_size[1]
-        x_mid_n = x_n - w_n/2
-        y_mid_n = y_n - h_n/2
+        x_mid_n = x_n + w_n/2
+        y_mid_n = y_n + h_n/2
         return [x_mid_n, y_mid_n, w_n, h_n]
 
-def bbox_center(bbox, format):
-    x,y,w,h = bbox_to_xywh(bbox, format)
-    return x+w/2, y+h/2
+def bbox_center(bbox, from_format, image_size=(800, 600)):
+    x_center, y_center, *_ = convert_bbox(bbox, from_format, BBox.XMIDYMIDWH_NORM, image_size=image_size)
+    return x_center, y_center
+
+if __name__ == '__main__':
+    bbox = [390, 290, 20, 20]
+    print(convert_bbox(bbox, from_format=BBox.XYWH, to_format=BBox.XMIDYMIDWH_NORM))
+
+    print(bbox_center(bbox, from_format=BBox.XYWH))
